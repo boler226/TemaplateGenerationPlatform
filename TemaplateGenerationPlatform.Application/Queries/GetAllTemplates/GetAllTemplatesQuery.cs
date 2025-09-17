93d3b4696a911fd29e83301d9d75e29d7ec93cea
Illya <illamatviuk@gmail.com>
2025-09-17 20:17:57 +0300

    Added GetAllTemplatesQuery.

---
+++ TemaplateGenerationPlatform.Application/Queries/GetAllTemplates/GetAllTemplatesQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TemaplateGenerationPlatform.Application.DTOs.Template;
+
+namespace TemaplateGenerationPlatform.Application.Queries.GetAllTemplates
+{
+    public record GetAllTemplatesQuery() : IRequest<List<TemplateDto>>;
+}
---
+++ TemaplateGenerationPlatform.Application/Queries/GetAllTemplates/GetAllTemplatesQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MediatR;
+using TemaplateGenerationPlatform.Application.DTOs.Template;
+using TemaplateGenerationPlatform.Domain.Entity;
+using TemaplateGenerationPlatform.Domain.Interfaces.Repositories;
+
+namespace TemaplateGenerationPlatform.Application.Queries.GetAllTemplates
+{
+    public class GetAllTemplatesQueryHandler(IRepository<TemplateEntity> repository, IMapper mapper) : IRequestHandler<GetAllTemplatesQuery, List<TemplateDto>>
+    {
+        public async Task<List<TemplateDto>> Handle(GetAllTemplatesQuery query, CancellationToken cancellationToken)
+        {
+            var templates = await repository.GetAllAsync(cancellationToken);
+
+            return mapper.Map<List<TemplateDto>>(templates);
+        }
+    }
+}
