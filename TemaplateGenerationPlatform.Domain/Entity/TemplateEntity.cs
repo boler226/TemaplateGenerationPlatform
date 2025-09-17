f32dba6da3865e697f1af3dbc1eabc6267cb654e
Illya <illamatviuk@gmail.com>
2025-09-17 12:06:37 +0300

    Add project files.

---
+++ TemaplateGenerationPlatform.Domain/Entity/TemplateEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace TemaplateGenerationPlatform.Domain.Entity
+{
+    public class TemplateEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string HtmlContent { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ TemaplateGenerationPlatform.Domain/Interfaces/Repositories/ITemplateRepository.cs
@@ -0,0 +1,13 @@
+﻿using TemaplateGenerationPlatform.Domain.Entity;
+
+namespace TemaplateGenerationPlatform.Domain.Interfaces.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
