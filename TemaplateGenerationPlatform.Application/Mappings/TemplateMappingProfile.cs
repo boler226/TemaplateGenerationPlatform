a076b7a857b6b932dd067060656a53980b293df8
Illya <illamatviuk@gmail.com>
2025-09-17 20:17:34 +0300

    Added TemplateMappingProfile.

---
+++ TemaplateGenerationPlatform.Application/Mappings/TemplateMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TemaplateGenerationPlatform.Application.DTOs.Template;
+using TemaplateGenerationPlatform.Domain.Entity;
+
+namespace TemaplateGenerationPlatform.Application.Mappings
+{
+    public class TemplateMappingProfile : Profile
+    {
+        public TemplateMappingProfile()
+        { 
+            CreateMap<TemplateEntity, TemplateDto>();
+        }
+    }
+}
