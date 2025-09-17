cf599d3b30ec72b0a3da547f75dbb1d48789ae74
Illya <illamatviuk@gmail.com>
2025-09-17 20:16:29 +0300

    Added AppDbContext.

---
+++ TemaplateGenerationPlatform.Infrastructure/DbContext/AppDbContext .cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TemaplateGenerationPlatform.Domain.Entity;
+
+namespace TemaplateGenerationPlatform.Infrastructure.DbContext
+{
+    public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<TemplateEntity> Templates { get; set; }
+    }
+}
