37ab0e9fdb8117bb7d4a5a6f27051ecc83049b00
Illya <illamatviuk@gmail.com>
2025-09-17 20:17:23 +0300

    Added TemplateDto.

---
+++ TemaplateGenerationPlatform.Application/DTOs/Template/TemplateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TemaplateGenerationPlatform.Application.DTOs.Template
+{
+    public class TemplateDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string HtmlContent { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
