f4c11aa17455a321d659620c53dfe05bbe9d0c4f
Illya <illamatviuk@gmail.com>
2025-09-18 21:33:59 +0300

    Added routes.

--- template-generation-client/src/App.jsx
+++ template-generation-client/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import TemplatesPage from "./pages/template/TemplatesPage.tsx";
 
 function App() {
 
   return (
-    <>
-
-    </>
+      <Routes>
+        <Route path="/" element={<TemplatesPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
   )
 }
 
