0de6e32667c84851ba0c1519280edc60a5d04f9c
Illya <illamatviuk@gmail.com>
2025-09-18 12:56:14 +0300

    Changed TemplatesController, added post Generate pdf.

--- TemaplateGenerationPlatform.API/Controllers/TemplatesController.cs
+++ TemaplateGenerationPlatform.API/Controllers/TemplatesController.cs
@@ -1,8 +1,10 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using TemaplateGenerationPlatform.Application.Commands.DeleteTemplate;
+using TemaplateGenerationPlatform.Application.Commands.GeneratePdf;
 using TemaplateGenerationPlatform.Application.Commands.Template;
 using TemaplateGenerationPlatform.Application.Commands.UpdateTemplate;
+using TemaplateGenerationPlatform.Application.DTOs.Template;
 using TemaplateGenerationPlatform.Application.Queries.GetAllTemplates;
 
 
@@ -16,12 +18,16 @@ namespace TemaplateGenerationPlatform.API.Controllers
         public async Task<IActionResult> GetAll() => 
             Ok(await mediator.Send(new GetAllTemplatesQuery()));
 
+        [HttpPost("{id:guid}/generate")]
+        public async Task<IActionResult> Generate(Guid id, [FromBody] Dictionary<string, string> data) =>
+            Ok(await mediator.Send(new GeneratePdfCommand(id, data)));
+
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateTemplateCommand command) => 
             Ok(await mediator.Send(command));
 
         [HttpPut("{id:guid}")]
-        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateTemplateCommand command) => 
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateTemplateDto command) => 
             Ok(await mediator.Send(new UpdateTemplateCommand(id, command.Name, command.HtmlContent)));
         
 
